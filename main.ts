957ea384da13639b7044b74611f60c8bd6e98e81
roseyracer <rinjuneevans@gmail.com>
2024-11-16 12:52:00 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(4)
+})
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showNumber(1)
+})
+input.onGesture(Gesture.ScreenUp, function () {
+    basic.showNumber(5)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showNumber(2)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(3)
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.showNumber(0)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showNumber(6)
+})
